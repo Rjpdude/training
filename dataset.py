499255856873fb67138b71632a0ba4de4918c584
Jeremy Pridgen <48602805+Rjpdude@users.noreply.github.com>
2024-02-03 16:40:38 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -25,3 +25,4 @@ if __name__ == "__main__":
     pipe = translator()
     dataset = load_dataset("teknium/OpenHermes-2.5")["train"]
     dataset = dataset.map(lambda x: {"messages": map_object(x["conversations"], pipe)}, remove_columns=["conversations"], batched=True)
+    dataset.push_to_hub("SiguienteGlobal/OpenHermes-2.5-es")
