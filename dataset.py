5ae50683041324020e10ec63826611d118008b4b
Jeremy Pridgen <48602805+Rjpdude@users.noreply.github.com>
2024-02-03 18:28:51 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -3,11 +3,6 @@ import json
 from datasets import load_dataset
 from transformers import pipeline
 
-
-def translator():
-    return pipeline("translation", model="Helsinki-NLP/opus-mt-en-es")
-
-
 def map_object(obj, func):
     """ Function to map each value in the object through the lambda function """
     if isinstance(obj, str):
@@ -15,23 +10,34 @@ def map_object(obj, func):
             obj = json.loads(obj)
         except json.JSONDecodeError:
             raise ValueError("Invalid JSON format")
-
-    mapped_obj = {}
-    for key, value in dict(obj).items():
-        mapped_obj[key] = func(value)
-    return mapped_obj
+    def message(row, pipe):
+        return {"role":"user" if row["from"] == "human" else "assistant", "content":pipe(row["value"])[0]["translation_text"]}
+    convo = [
+        message(row, func) for row in obj
+    ]
+    return convo
 
 
 def map_row(pipeline):
     def mapper(row):
         return dict(
-            conversations=map_object(row, pipeline)
+            conversations=[
+                map_object(x, pipeline) for x in row["conversations"]
+            ]
         )
 
     return mapper
 
 
 if __name__ == "__main__":
-    dataset = load_dataset("teknium/OpenHermes-2.5", split="train")
-    dataset = dataset.map(map_row(translator()), batched=True)
-    dataset.push_to_hub("SiguienteGlobal/OpenHermes-2.5-es")
+    transform = map_row(
+        pipeline("translation", model="Helsinki-NLP/opus-mt-en-es", max_length=32000)
+    )
+    def batch_transform(vals):
+        try:
+            return transform(vals)
+        except:
+            pass
+    dataset = load_dataset("teknium/OpenHermes-2.5")
+    dataset = dataset.map(batch_transform, batched=True)
+    dataset.push_to_hub("SiguienteGlobal/hermes-es")
