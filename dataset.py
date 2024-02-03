38dcffe8e0b891a1fb179fab393896807ba04481
Jeremy Pridgen <48602805+Rjpdude@users.noreply.github.com>
2024-02-03 16:38:23 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,76 +1,27 @@
 import json
 
-import torch
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForCausalLM, LocalAgent
+from transformers import pipeline
 
-model = AutoModelForCausalLM.from_pretrained("NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO", device_map="auto",
-                                             torch_dtype=torch.bfloat16)
-tokenizer = AutoTokenizer.from_pretrained("NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO")
-
-capy = load_dataset("argilla/distilabel-capybara-dpo-9k-binarized", split="train")
-
-capy = capy.filter(
-  lambda r: r["rating_chosen"]>=4
-)
-
-capy = capy.map(lambda r: {"messages": len(r["chosen"])}).filter(lambda r: r["messages"]<18)
-
-def chatml_format(example):
-    # get everything except the last message as input
-    prompt = tokenizer.apply_chat_template(example["chosen"][:-1], tokenize=False, add_generation_prompt=True)
-    # get the last assistant responses
-    chosen = example["chosen"][-1]["content"] + "</s>" 
-    rejected = example["rejected"][-1]["content"] + "</s>" 
-
-    return {
-        "prompt": system + prompt,
-        "chosen": chosen,
-        "rejected": rejected,
-    }
+def translator():
+    return pipeline("translation", model="Helsinki-NLP/opus-mt-en-es")
 
 
-# Save columns
-original_columns = capy.column_names
+def map_object(obj, func):
+    """ Function to map each value in the object through the lambda function """
+    if isinstance(obj, str):
+        try:
+            obj = json.loads(obj)
+        except json.JSONDecodeError:
+            raise ValueError("Invalid JSON format")
 
-# Format dataset
-capy = capy.map(
-    chatml_format,
-    remove_columns=original_columns
-).to_json("transformed.json")
-
-# capy = load_dataset("argilla/distilabel-capybara-dpo-9k-binarized", split="train")
-#
-
-# capy = capy.filter(
-#     lambda r: r["rating_chosen"] >= 4
-# )
-#
-# capy = capy.map(lambda r: {"messages": len(r["chosen"])}).filter(lambda r: r["messages"] < 18)
+    mapped_obj = {}
+    for key, value in obj.items():
+        mapped_obj[key] = func(value)
+    return mapped_obj
 
 
-# def chatml_format(example):
-#     example["chosen"] = agent.chat(
-#         f"Translate the following from spanish into a mexican dialect of spanish: {example['chosen']}")
-#     return example
-#     # system = ""
-#     # # get everything except the last message as input
-#     # prompt = tokenizer.apply_chat_template(example["chosen"][:-1], tokenize=False, add_generation_prompt=True)
-#     # # get the last assistant responses
-#     # chosen = example["chosen"][-1]["content"] + "</s>"
-#     # rejected = example["rejected"][-1]["content"] + "</s>"
-#     #
-#     # return {
-#     #     "prompt": agent.run_prompt_template(),
-#     #     "chosen": agent.run(chosen),
-#     #     "rejected": agent.run(rejected),
-#     # }
-#
-#
-# # Save columns
-# original_columns = capy.column_names
-#
-# # Format dataset
-# capy = capy.map(chatml_format, remove_columns=original_columns, batched=True)
-
-# capy.to_json("formatted.json")
+if __name__ == "__main__":
+    pipe = translator()
+    dataset = load_dataset("teknium/OpenHermes-2.5")["train"]
+    dataset = dataset.map(lambda x: {"messages": map_object(x["conversations"], pipe)}, remove_columns=["conversations"], batched=True)
