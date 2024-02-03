425dac7bc2ad38703cd74baa241a01fd161836f0
Jeremy Pridgen <48602805+Rjpdude@users.noreply.github.com>
2024-02-03 12:01:43 -0500

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,43 @@
+import json
+
+import torch
+from datasets import load_dataset
+from transformers import AutoTokenizer, load_tool, AutoModelForCausalLM, LocalAgent
+
+model = AutoModelForCausalLM.from_pretrained("NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO", device_map="auto", torch_dtype=torch.bfloat16)
+tokenizer = AutoTokenizer.from_pretrained("NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO")
+agent = LocalAgent(model, tokenizer, run_prompt_template="Act as a translator. Given an input in english, output a contextualized version in a mexican dialect of spanish.")
+
+translator = load_tool("translation", "Helsinki-NLP/opus-mt-en-es")
+tokenizer = AutoTokenizer.from_pretrained("meetkai/functionary-medium-v2.2")
+capy = load_dataset("argilla/distilabel-capybara-dpo-9k-binarized", split="train")
+
+capy = capy.filter(
+    lambda r: r["rating_chosen"] >= 4
+)
+
+capy = capy.map(lambda r: {"messages": len(r["chosen"])}).filter(lambda r: r["messages"] < 18)
+
+
+def chatml_format(example):
+    system = ""
+    # get everything except the last message as input
+    prompt = tokenizer.apply_chat_template(example["chosen"][:-1], tokenize=False, add_generation_prompt=True)
+    # get the last assistant responses
+    chosen = example["chosen"][-1]["content"] + "</s>"
+    rejected = example["rejected"][-1]["content"] + "</s>"
+
+    return {
+        "prompt": agent.run(system + prompt),
+        "chosen": agent.run(chosen),
+        "rejected": agent.run(rejected),
+    }
+
+
+# Save columns
+original_columns = capy.column_names
+
+# Format dataset
+capy = capy.map(chatml_format, remove_columns=original_columns, batched=True)
+
+capy.to_json("formatted.json")
