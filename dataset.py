1dc790bd8b8f5e3d1a1d37e5ea39f95f46f64895
Jeremy Pridgen <48602805+Rjpdude@users.noreply.github.com>
2024-02-04 12:12:59 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -16,7 +16,7 @@ class Source:
     def __post_init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained(self.path, padding_side="left")
         self.tokenizer.pad_token = self.tokenizer.eos_token 
-        self.model = AutoModelForCausalLM.from_pretrained(self.model).to("cuda")
+        self.model = AutoModelForCausalLM.from_pretrained(self.path).to("cuda")
         return self
 
     def generate(self, input):
