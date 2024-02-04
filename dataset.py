8c2834493b0e970dbb74559d28d94a080041aab9
Jeremy Pridgen <48602805+Rjpdude@users.noreply.github.com>
2024-02-03 19:20:07 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,50 +1,65 @@
-import json
+# Copyright 2023-present, Argilla, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import time
 
 from datasets import load_dataset
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
-from accelerate import Accelerator
-
-def map_object(obj, func):
-    """ Function to map each value in the object through the lambda function """
-    if isinstance(obj, str):
-        try:
-            obj = json.loads(obj)
-        except json.JSONDecodeError:
-            raise ValueError("Invalid JSON format")
-    def message(row, pipe):
-        return {"role":"user" if row["from"] == "human" else "assistant", "content":pipe(row["value"])[0]}
-    convo = [
-        message(row, func) for row in obj
-    ]
-    return convo
-
-
-def map_row(pipeline):
-    def mapper(row):
-        return dict(
-            conversations=[
-                map_object(x, pipeline) for x in row["conversations"]
-            ]
-        )
-
-    return mapper
-
+from distilabel.llm import InferenceEndpointsLLM
+from distilabel.pipeline import pipeline
+from distilabel.tasks import TextGenerationTask
 
 if __name__ == "__main__":
-    accelerator = Accelerator()
-    pipe = pipeline(
-        "text-generation",
-        model="NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO",
-        device_map="auto"
+    dataset = (
+        load_dataset("teknium/OpenHermes-2.5", split="test[:100]")
+        .prepare_for_task("text-generation")
+        .rename_column("conversations", "input")
     )
-    def flow(input):
-        return pipe(f"Act as a translator and transform the following into spanish: {input}")
-    transform = map_row(flow)
-    def batch_transform(vals):
-        try:
-            return transform(vals)
-        except:
-            pass
-    dataset = load_dataset("teknium/OpenHermes-2.5")
-    dataset = dataset.map(batch_transform, batched=True)
-    dataset.push_to_hub("SiguienteGlobal/hermes-es")
+
+    pipe = pipeline(
+        "preference",
+        "text-quality",
+        generator=InferenceEndpointsLLM(
+            endpoint_name_or_model_id="NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO",  # type: ignore
+            task=TextGenerationTask(system_prompt="Act as a translator, converting english inputs into pure "
+                                                  "mexican-dialect spanish."),
+            prompt_format="chatml",
+            max_new_tokens=4092,
+            num_threads=16,
+            temperature=1.2,
+        ),
+        max_new_tokens=256,
+        num_threads=2,
+        api_key="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",
+        temperature=0.0,
+    )
+
+    start = time.time()
+    dataset = pipe.generate(
+        dataset,  # type: ignore
+        num_generations=2,
+        batch_size=1,
+        checkpoint_strategy=True,
+        display_progress_bar=True,
+    )
+    end = time.time()
+    print("Elapsed", end - start)
+
+    # Push to the HuggingFace Hub
+    dataset.push_to_hub(
+        "SiguienteGlobal/hermes-es",  # type: ignore
+        split="train",
+        private=True,
+        token="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",
+    )
